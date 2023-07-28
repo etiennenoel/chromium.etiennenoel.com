93a1f780ee5c807fb5972740395c7c7fa14c5f13
Etienne Noel <etienne.noel@ima-tech.ca>
2023-07-28 10:23:27 -0700

    - Update.

---
+++ opfs/opfs.ts
@@ -0,0 +1,49 @@
+const buildFileSystem = async (paths: string[]): Promise<void> => {
+    const root = await navigator.storage.getDirectory();
+
+    for (const path of paths) {
+
+        const splitPath = path.split("/");
+        let parent = root;
+
+        let index = 0;
+        for (const fileOrFolder of splitPath) {
+            if(index === splitPath.length - 1) {
+                await parent.getFileHandle(fileOrFolder, {create: true});
+            } else {
+                parent = await parent.getDirectoryHandle(fileOrFolder, {create: true})
+            }
+
+            index++;
+        }
+    }
+}
+
+const buildTree = async (directory) => {
+    let html = "<ul>";
+
+    for await (const handle of directory.values()) {
+        if(handle.kind === "directory") {
+            html += "<li>" + handle.name + " " + await buildTree(handle) +"</li>";
+        } else if(handle.kind === "file") {
+            html += "<li>" + handle.name + "</li>";
+        }
+
+    }
+
+    html += "</ul>"
+
+    return html;
+}
+
+buildFileSystem([
+    "a/b/c/d.txt",
+    "animals/dogs/peach.txt",
+    "animals/dogs/melchior.txt",
+    "animals/cats/sir-winston-churchill.txt",
+]).then(async () => {
+    const root = await navigator.storage.getDirectory();
+
+    document.getElementById("filesystem").innerHTML = await buildTree(root);
+})
+
---
+++ web-sqlite/3.41.2/web-sqlite.js
@@ -0,0 +1,63 @@
+var SqliteMessageTypeEnum;
+(function (SqliteMessageTypeEnum) {
+    SqliteMessageTypeEnum["CreateDatabase"] = "CREATE_DATABASE";
+    SqliteMessageTypeEnum["CreateDatabaseResult"] = "CREATE_DATABASE_RESULT";
+    SqliteMessageTypeEnum["ExecuteSql"] = "EXECUTE_SQL";
+    SqliteMessageTypeEnum["ExecuteSqlResult"] = "EXECUTE_SQL_RESULT";
+})(SqliteMessageTypeEnum || (SqliteMessageTypeEnum = {}));class ExecuteSqlMessage {
+    constructor(sqlStatement, bindingParameters = []) {
+        this.sqlStatement = sqlStatement;
+        this.bindingParameters = bindingParameters;
+        this.type = SqliteMessageTypeEnum.ExecuteSql;
+        this.uniqueId = crypto.randomUUID();
+    }
+}class CreateDatabaseMessage {
+    constructor(filename) {
+        this.filename = filename;
+        this.type = SqliteMessageTypeEnum.CreateDatabase;
+        this.uniqueId = crypto.randomUUID();
+    }
+}class WebSqlite {
+    constructor(filename, sqliteWorkerPath) {
+        this.filename = filename;
+        this.sqliteWorkerPath = sqliteWorkerPath;
+        this.queuedPromises = {};
+    }
+    init() {
+        this.worker = new Worker(this.sqliteWorkerPath, {
+            type: "module",
+        });
+        this.worker.onmessage = this.messageReceived.bind(this);
+        const createDatabaseMessage = new CreateDatabaseMessage(this.filename);
+        this.worker.postMessage(createDatabaseMessage);
+        return new Promise((resolve, reject) => {
+            this.queuedPromises[createDatabaseMessage.uniqueId] = {
+                resolve,
+                reject,
+            };
+        });
+    }
+    messageReceived(message) {
+        const sqliteMessage = message.data;
+        if (sqliteMessage.uniqueId !== undefined && this.queuedPromises.hasOwnProperty(sqliteMessage.uniqueId)) {
+            const promise = this.queuedPromises[sqliteMessage.uniqueId];
+            delete this.queuedPromises[sqliteMessage.uniqueId];
+            switch (sqliteMessage.type) {
+                case SqliteMessageTypeEnum.ExecuteSqlResult:
+                    return promise.resolve(sqliteMessage.result);
+                case SqliteMessageTypeEnum.CreateDatabaseResult:
+                    return promise.resolve();
+            }
+        }
+    }
+    executeSql(sqlStatement, bindParameters = []) {
+        const executeSqlMessage = new ExecuteSqlMessage(sqlStatement, bindParameters);
+        this.worker.postMessage(executeSqlMessage);
+        return new Promise((resolve, reject) => {
+            this.queuedPromises[executeSqlMessage.uniqueId] = {
+                resolve,
+                reject,
+            };
+        });
+    }
+}export{WebSqlite};
